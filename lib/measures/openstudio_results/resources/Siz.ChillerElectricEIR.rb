c450bcbfcea4167b289eb3614df75958484b91b2
Matt Steen <matthew.steen@nrel.gov>
2025-10-23 12:44:37 -0400

    fix 2

--- lib/measures/openstudio_results/resources/Siz.ChillerElectricEIR.rb
+++ lib/measures/openstudio_results/resources/Siz.ChillerElectricEIR.rb
@@ -62,7 +62,7 @@ class OpenStudio::Model::ChillerElectricEIR
     if Gem::Version.new(OpenStudio.openStudioVersion) < Gem::Version.new('1.5.4')
       effs << [compressorMotorEfficiency, 'Compressor Motor Efficiency']
     else
-      effs << [compressorMotorEfficiency, 'Compressor Motor Fraction of Compressor Electric Consumption Rejected by Condenser']
+      effs << [fractionofCompressorElectricConsumptionRejectedbyCondenser, 'Fraction of Compressor Electric Power Rejected by Condenser']
     end
     return effs
   end
